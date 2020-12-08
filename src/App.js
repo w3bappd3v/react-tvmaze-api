60aea8dcdbc447b3869946f7321d47fcc218e616
Rob R <r0brh0d3s@gmail.com>
2020-12-08 14:37:49 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Show } from './components/Show.js';
+import { ShowDetails } from './components/ShowDetails.js';
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = { 
+      shows: [],
+      searchStr: '',
+      showDetails: false,
+      show: {}
+    }
+  }
+
+  updateSearchStr(e) {
+    this.setState({ searchStr: e.target.value });
+  }
+
+  search () {
+    this.setState({ showDetails: false });
+    fetch("http://api.tvmaze.com/search/shows?q=" + this.state.searchStr).then(response => {
+      return response.json();
+    }).then(data => {
+      this.setState({ shows: data });
+    });
+  }
+
+  goToShow (showId) {
+    this.setState({ showDetails: true });
+    fetch("http://api.tvmaze.com/shows/" + showId).then(response => {
+      return response.json();
+    }).then(data => {
+      this.setState({ show: data });
+    });
+  }
+
+  render () {
+    return (
+      <div className="app">
+        <div class="header">Show Finder</div>
+        <div class="content">
+          <div class="search-control-row">
+            <input type="text" onChange={ this.updateSearchStr.bind(this) } />
+            <button onClick={ this.search.bind(this) }>Search</button>
+          </div>
+          {
+            this.state.showDetails
+            ? (
+              <p>
+                <ShowDetails 
+                    name={ this.state.show.name } />
+              </p>
+            )
+            : (
+              <div className="shows">
+              {this.state.shows.map(show => {
+                if(show.show.image) {
+                  return <Show goToShow={ this.goToShow.bind(this) } 
+                            key={`show-${show.show.id}`}
+                            name={show.show.name}
+                            id={show.show.id}
+                            image={show.show.image.medium} 
+                            summary={show.show.summary} />
+                } else {
+                  return <Show goToShow={ this.goToShow.bind(this) } 
+                  key={`show-${show.show.id}`}
+                  name={show.show.name}
+                  id={show.show.id} 
+                  summary={show.show.summary} />
+                }
+              })}
+            </div>
+            )
+          }
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
